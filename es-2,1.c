978f5955d34813b145fe3ee3a7fb7a5ffaa110c9
Andrea Di Mingo <dimingo.andrea25@gmail.com>
2024-09-20 18:16:39 +0200

    Scritto il programma per l'esercizio

---
+++ es-2,1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float primonumero;
+    float secondonumero;
+    float media;
+
+    printf("Inserisci il primo numero di cui fare la media\n");
+    scanf("%f", &primonumero);
+    printf("Inserisci il secondo numero di cui vuoi fare la media\n");
+    scanf("%f", &secondonumero);
+
+    media = (primonumero + secondonumero)/2;
+    printf("La media dei due numeri è %f\n", media);
+    getchar();
+}
